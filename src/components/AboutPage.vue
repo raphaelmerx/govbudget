fb31f0c5668f3bbdf7db5c8c031c6e69e7b2cf58
Raphael Merx <raphael.merx@gmail.com>
2020-10-04 14:06:35 +0200

    Form add Send button

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -51,8 +51,9 @@
             <b-form-input type="email" required placeholder="Your email"></b-form-input>
           </b-form-group>
           <b-form-group id="input-group-1" label-for="input-1" >
-            <b-form-textarea required placeholder="Comments"></b-form-textarea>
+            <b-form-textarea required placeholder="Comments" rows="5"></b-form-textarea>
           </b-form-group>
+          <b-button type="submit" variant="primary">Send</b-button>
         </b-form>
       </p>
   </b-container>
