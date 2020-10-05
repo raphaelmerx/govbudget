c22cceb94a8b3e1f5d3807813fb63d4ef8b54ac7
Raphael Merx <raphael.merx@gmail.com>
2020-10-05 18:07:06 +0200

    Add server prerendering
    To get form preprocessed by Netlify

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
+module.exports = {
+  configureWebpack: () => {
+    if (process.env.NODE_ENV !== 'production') return;
+    return {
+      plugins: [
+        new PrerenderSPAPlugin({
+          staticDir: path.join(__dirname, 'dist'),
+          routes: ['/',],
+        }),
+      ]
+    }
+  }
+}
