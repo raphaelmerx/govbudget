aa3249bb2e6660ee9dfb9861d22f241a16f02661
Raphael Merx <raphael.merx@gmail.com>
2021-07-10 13:41:06 +0800

    Refactor constructing table data

--- src/components/TableComparison.js
+++ src/components/TableComparison.js
@@ -6,40 +6,45 @@ import { DataGrid } from '@material-ui/data-grid';
 import {getFormattedGraphData} from '../helpers/financials'
 
 
+const getTableRows = (country1, country2) => {
+  const graphData = {
+    'per-capita-1': getFormattedGraphData(country1, "nominal-per-capita-usd", null),
+    'per-capita-2': getFormattedGraphData(country2, "nominal-per-capita-usd", null),
+    'gdp-1': getFormattedGraphData(country1, "percentGDP", null),
+    'gdp-2': getFormattedGraphData(country2, "percentGDP", null),
+  }
+  const categories = graphData['per-capita-1'].children.map(c => c.name)
+
+  const tableRows = categories.map(categoryName => {
+    const rowData = {
+      id: categoryName,
+      name: categoryName,
+    }
+    for (let [key, countryGraph] of Object.entries(graphData)) {
+      let categoryGraph = countryGraph.children.filter(c => c.name === categoryName)[0];
+      // value already present in the category, push it to the row
+			if (categoryGraph.value !== undefined) rowData[key] = categoryGraph.value;
+      // else sum the value of all subcategory values, push it to the row
+      else rowData[key] = categoryGraph.children.reduce((prev, current) => prev + current.value, 0)
+    }
+    return rowData
+  })
+
+  return tableRows
+}
+
 export default function TableComparison() {
   const [selectedCountry1, selectedCountry2] = useSelector((state) => [state.country.value, state.country2.value])
 
-  const graphData = [
-    getFormattedGraphData(selectedCountry1, "nominal-per-capita-usd", null),
-    getFormattedGraphData(selectedCountry2, "nominal-per-capita-usd", null),
-    getFormattedGraphData(selectedCountry1, "percentGDP", null),
-    getFormattedGraphData(selectedCountry2, "percentGDP", null),
-  ]
-
-  const tableRows = graphData[1].children.map(c => {
-    const name = c.name;
-    const rowValues = graphData.map(e => {
-      const categoryTree = e.children.filter(c => c.name === name)[0];
-			if (categoryTree.value !== undefined) return categoryTree.value;
-      return categoryTree.children.reduce((prev, current) => prev + current.value, 0)
-    })
-    return {
-      id: name,
-      name: name,
-      'cap1': rowValues[0],
-      'cap2': rowValues[1],
-      'gdp1': rowValues[2],
-      'gdp2': rowValues[3],
-    }
-  })
-
   const columns = [
     { field: 'name', headerName: 'Category', flex: 1 },
-    { field: 'cap1', headerName: `${selectedCountry1} / capita (USD)`, flex: 1, valueFormatter: (params) => params.value.toLocaleString()},
-    { field: 'cap2', headerName: `${selectedCountry2} / capita (USD)`, flex: 1, valueFormatter: (params) => params.value.toLocaleString() },
-    { field: 'gdp1', headerName: `${selectedCountry1} % GDP`, flex: 1, valueFormatter: (params) => params.value.toFixed(2) },
-    { field: 'gdp2', headerName: `${selectedCountry2} % GDP`, flex: 1, valueFormatter: (params) => params.value.toFixed(2)  },
+    { field: 'per-capita-1', headerName: `${selectedCountry1} / capita (USD)`, flex: 1, valueFormatter: (params) => params.value.toLocaleString()},
+    { field: 'per-capita-2', headerName: `${selectedCountry2} / capita (USD)`, flex: 1, valueFormatter: (params) => params.value.toLocaleString() },
+    { field: 'gdp-1', headerName: `${selectedCountry1} % GDP`, flex: 1, valueFormatter: (params) => params.value.toFixed(2) },
+    { field: 'gdp-2', headerName: `${selectedCountry2} % GDP`, flex: 1, valueFormatter: (params) => params.value.toFixed(2)  },
   ]
+
+  const tableRows = getTableRows(selectedCountry1, selectedCountry2)
 
   return (
     <div style={{ width: '100%', minWidth: '900px' }}>
