ebdaa9a87713dd791eac51a25c7b6dbcdb65e4d6
Raphaël Merx <raphaelmerx@users.noreply.github.com>
2021-07-16 21:23:35 +0800

    Map: switch to react-simple-maps (#6)

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,11 +1,21 @@
+import React, { memo } from "react";
 import { useSelector, useDispatch } from 'react-redux'
 import Container from '@material-ui/core/Container';
-import { Chart } from 'react-google-charts'
+import {
+  ZoomableGroup,
+  ComposableMap,
+  Geographies,
+  Geography,
+} from "react-simple-maps";
+import { scaleLinear } from "d3-scale";
 
 import { selectOption } from '../reducers/categorySlice'
 import CategorySelector from '../components/CategorySelector';
 
 import {getCategorySpendPerCapitaUSD, getCategorySpendPercentGDP} from '../helpers/financials'
+
+const geoUrl = "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+
 
 const getCountryRows = (countries, category) => {
   return countries.map((country) => {
@@ -15,7 +25,7 @@ const getCountryRows = (countries, category) => {
   })
 }
 
-function Map() {
+const Map = ({ setTooltipContent }) => {
   let selectedCategory = useSelector((state) => state.category.value)
   const countryOptions = useSelector((state) => state.country.options)
   const dispatch = useDispatch()
@@ -26,6 +36,11 @@ function Map() {
 
   const rows = [['Country', 'Public spending / capita (USD)', 'Public spending as % of GDP'], ...getCountryRows(countryOptions, selectedCategory)]
 
+  const maxSpend = Math.max(...rows.slice(1, rows.length).map((row) => row[1]))
+  const colorScale = scaleLinear()
+    .domain([0, maxSpend])
+    .range(["#ffedea", "#0e8716"]);
+
   return (
     <Container id="about-container">
       <h1 id="header-main">
@@ -33,12 +48,54 @@ function Map() {
         <CategorySelector />
       </h1>
       <div>
-        <Chart
-          chartType="GeoChart"
-          data={rows}
-          mapsApiKey="YOUR_KEY_HERE"
-          rootProps={{ 'data-testid': '1' }}
-        />
+        <ComposableMap
+          height={400}
+          data-tip=""
+          projectionConfig={{
+            rotate: [-10, 0, 0],
+          }}
+        >
+          <ZoomableGroup>
+          {rows.length > 0 && (
+            <Geographies geography={geoUrl}>
+              {({ geographies }) =>
+                geographies.map((geo) => {
+                  const row = rows.find((r) => geo.properties.NAME.startsWith(r[0]));
+                  return (
+                    <Geography
+                      key={geo.rsmKey}
+                      geography={geo}
+                      fill={row ? colorScale(row[1]) : "#F5F4F6"}
+                      onMouseEnter={() => {
+                        if (!row) return
+                        const country = row[0]
+                        const amount = row[1].toLocaleString()
+                        const gdp = row[2]
+                        const content = `${country}: $${amount} / capita, ${gdp}% of GDP`
+
+                        setTooltipContent(content);
+                      }}
+                      onMouseLeave={() => {
+                        setTooltipContent("");
+                      }}
+                      style={{
+                        default: {
+                          outline: "none"
+                        },
+                        hover: {
+                          outline: "none"
+                        },
+                        pressed: {
+                          outline: "none"
+                        }
+                      }}
+                    />
+                  );
+                })
+              }
+            </Geographies>
+          )}</ZoomableGroup>
+        </ComposableMap>
       </div>
       <p>
         <em>
@@ -49,4 +106,4 @@ function Map() {
   );
 }
 
-export default Map;
+export default memo(Map);
