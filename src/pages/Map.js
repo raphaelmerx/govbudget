f0984f4ff86ece6fc217f267830d0b59c6025ce3
Raphael Merx <raphael.merx@gmail.com>
2021-07-16 16:42:09 +0800

    Initial globe with color coding

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,11 +1,26 @@
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from 'react-redux'
 import Container from '@material-ui/core/Container';
-import { Chart } from 'react-google-charts'
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Sphere,
+  Graticule
+} from "react-simple-maps";
+import { scaleLinear } from "d3-scale";
+import ReactTooltip from "react-tooltip";
 
 import { selectOption } from '../reducers/categorySlice'
 import CategorySelector from '../components/CategorySelector';
 
 import {getCategorySpendPerCapitaUSD, getCategorySpendPercentGDP} from '../helpers/financials'
+
+const geoUrl = "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+
+const colorScale = scaleLinear()
+  .domain([0, 20000])
+  .range(["#ffedea", "#ff5233"]);
 
 const getCountryRows = (countries, category) => {
   return countries.map((country) => {
@@ -33,12 +48,31 @@ function Map() {
         <CategorySelector />
       </h1>
       <div>
-        <Chart
-          chartType="GeoChart"
-          data={rows}
-          mapsApiKey="YOUR_KEY_HERE"
-          rootProps={{ 'data-testid': '1' }}
-        />
+        <ComposableMap
+          projectionConfig={{
+            rotate: [-10, 0, 0],
+            scale: 147
+          }}
+        >
+          <Sphere stroke="#E4E5E6" strokeWidth={0.5} />
+          <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
+          {rows.length > 0 && (
+            <Geographies geography={geoUrl}>
+              {({ geographies }) =>
+                geographies.map((geo) => {
+                  const row = rows.find((r) => r[0] === geo.properties.NAME);
+                  return (
+                    <Geography
+                      key={geo.rsmKey}
+                      geography={geo}
+                      fill={row ? colorScale(row[1]) : "#F5F4F6"}
+                    />
+                  );
+                })
+              }
+            </Geographies>
+          )}
+        </ComposableMap>
       </div>
       <p>
         <em>
